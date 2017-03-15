790618ee8edab252b844aa871efdcf52d6b22af7
mhnakashima <mhnakashima@gmail.com>
2017-03-14 22:59:23 -0300

    servicos, detalhes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 (function($){
    
+	$(document).ready(function(){
 
+		var headerHeightSize = 0;
+		var mainFixedMenuScroller = $(".mainFixedMenuScroller");
+
+		var getHeaderVal = function(){
+			return $("#header").height();
+		}
+
+		var showFixedHeader = function(showHeader){
+			if(showHeader){
+				mainFixedMenuScroller
+									.addClass('show');
+			}else{
+				mainFixedMenuScroller
+									.removeClass('show');
+			}
+		}
+
+		$(window).on('scroll', function(delta){
+			
+			if($(window).scrollTop() > headerHeightSize){
+				showFixedHeader(true);
+			}else{
+				showFixedHeader(false);
+			}
+		})
+
+
+		$(window).on('resize', function(){
+			headerHeightSize = getHeaderVal();
+		})
+
+		headerHeightSize = getHeaderVal();
+
+	})
 
 })(jQuery);
